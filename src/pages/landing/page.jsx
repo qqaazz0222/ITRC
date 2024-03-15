0e14472d83cf9a0380b891847ab3c0f3dd089363
qqaazz0222 <qqaazz0222@naver.com>
2024-03-15 13:08:01 +0900

    landing page work #3

--- src/pages/landing/page.jsx
+++ src/pages/landing/page.jsx
@@ -1,38 +1,28 @@
 // 라이브러리
+import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { useNavigate } from "react-router-dom";
 // 서비스
 // 컴포넌트
+import PageOut from "@/components/pageTransition/pageOut";
 // 아이콘
 // 이미지
+import ModelDetect from "/images/model-detect-w-640.png"
 // 스타일
+import "./animation.css";
 import "./style.css";
-import { useEffect, useState } from "react";
-import PageOut from "@/components/pageTransition/pageOut";
+
 const Landing = () => {
     const navigate = useNavigate();
     const [isOut, setIsOut] = useState(false);
     const [s2ref, s2InView] = useInView();
+    const [s2Contentref, s2ContentInView] = useInView();
     const section1BgHandler = () => {
         const target = document.querySelector("#landingPage .section.section1");
         window.addEventListener("scroll", function () {
             const y = (window.scrollY / window.innerHeight) * 150 + 50;
             target.style.backgroundPosition = `50% ${y}%`;
         });
-    };
-    const section2Handler = () => {
-        const contents = document.querySelectorAll(".section2 .content");
-        console.log(contents);
-        contents[0].style.display = "flex";
-        contents[1].style.display = "flex";
-        contents[2].style.display = "flex";
-        contents[3].style.display = "flex";
-        contents[4].style.display = "flex";
-        contents[0].style.animation = "contentSlideIn1 2.75s forwards";
-        contents[1].style.animation = "contentSlideIn2 3s forwards";
-        contents[2].style.animation = "contentSlideIn3 3s forwards";
-        contents[3].style.animation = "contentSlideIn4 2.5s forwards";
-        contents[4].style.animation = "contentSlideIn5 2.9s forwards";
     };
     const navigateHandler = (setState = () => {}, url = "") => {
         setState(true);
@@ -43,11 +33,6 @@ const Landing = () => {
     useEffect(() => {
         section1BgHandler();
     }, []);
-    useEffect(() => {
-        if (s2InView) {
-            section2Handler();
-        }
-    }, [s2InView]);
     return (
         <div id="landingPage" className="page">
             <div className="section section1">
@@ -81,16 +66,59 @@ const Landing = () => {
                         점점 더 발전하는 인공지능이 앞으로 우리의 삶에 어떻게
                         사용될지 알아봐요.
                     </div>
-                    <div className="contentWrap">
-                        <div className="content"></div>
-                        <div className="content"></div>
-                        <div className="content"></div>
-                        <div className="content"></div>
-                        <div className="content"></div>
-                        <div className="content"></div>
-                        <div className="content"></div>
-                        <div className="content"></div>
-                    </div>
+                    {(s2InView || s2ContentInView) && (
+                        <div className="contentWrap" ref={s2Contentref}>
+                            <div className="content">
+                                <div className="contentWrap">
+                                    <div className="contentHeader">
+                                        <div className="tags">
+                                            <div className="tag">test</div>
+                                        </div>
+                                        <div
+                                            className="link"
+                                            onClick={() => {
+                                                navigateHandler(
+                                                    setIsOut,
+                                                    "/detail"
+                                                );
+                                            }}
+                                        >
+                                            <svg
+                                                width="24"
+                                                height="24"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                            >
+                                                <path
+                                                    d="M7 5H19V17"
+                                                    stroke="white"
+                                                    stroke-width="2"
+                                                />
+                                                <path
+                                                    d="M5 19L19 5"
+                                                    stroke="white"
+                                                    stroke-width="2"
+                                                />
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div className="desc">
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit.
+                                    </div>
+                                </div>
+                            </div>
+                            <ContentCard thumb={ModelDetect} tags={["태그1" , "태그2"]} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit."} navigateHandler={navigateHandler} setIsOut={setIsOut}/>
+                            <ContentCard tags={["태그1" , "태그2"]} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit."} navigateHandler={navigateHandler} setIsOut={setIsOut}/>
+                            <ContentCard tags={["태그1" , "태그2"]} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit."} navigateHandler={navigateHandler} setIsOut={setIsOut}/>
+                            <ContentCard tags={["태그1" , "태그2"]} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit."} navigateHandler={navigateHandler} setIsOut={setIsOut}/>
+                            <ContentCard tags={["태그1" , "태그2"]} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit."} navigateHandler={navigateHandler} setIsOut={setIsOut}/>
+                            <ContentCard tags={["태그1" , "태그2"]} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit."} navigateHandler={navigateHandler} setIsOut={setIsOut}/>
+                            <ContentCard tags={["태그1" , "태그2"]} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit."} navigateHandler={navigateHandler} setIsOut={setIsOut}/>
+                            <ContentCard tags={["태그1" , "태그2"]} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit."} navigateHandler={navigateHandler} setIsOut={setIsOut}/>
+                            
+                        </div>
+                    )}
                 </div>
             </div>
             <div className="section section3">
@@ -103,6 +131,55 @@ const Landing = () => {
                 </button>
             </div>
             {isOut && <PageOut />}
+        </div>
+    );
+};
+
+const ContentCard = ({
+    thumb = null,
+    tags = [],
+    desc = [],
+    navigateHandler = () => {},
+    setIsOut = () => {},
+}) => {
+    return (
+        <div className="content" style={{backgroundImage: `${thumb}`}}>
+            <div className="contentWrap">
+                <div className="contentHeader">
+                    <div className="tags">
+                        {tags.map((tag, idx) => (
+                            <div className="tag" key={`tag${idx}`}>
+                                {tag}
+                            </div>
+                        ))}
+                    </div>
+                    <div
+                        className="link"
+                        onClick={() => {
+                            navigateHandler(setIsOut, "/detail");
+                        }}
+                    >
+                        <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                        >
+                            <path
+                                d="M7 5H19V17"
+                                stroke="white"
+                                stroke-width="2"
+                            />
+                            <path
+                                d="M5 19L19 5"
+                                stroke="white"
+                                stroke-width="2"
+                            />
+                        </svg>
+                    </div>
+                </div>
+                <div className="desc">{desc}</div>
+            </div>
         </div>
     );
 };
